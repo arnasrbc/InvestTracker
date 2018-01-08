5f3f27e8437e27b586c153d57547a56ea1776430
arnasrbc <Atax7ma1>
2018-01-08 21:49:38 +0200

    Added list entities by search.

--- src/pages/entities/entities.ts
+++ src/pages/entities/entities.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {ElasticsearchProvider} from "../../providers/elasticsearch/elasticsearch";
+import { ElasticsearchProvider } from "../../providers/elasticsearch/elasticsearch";
+import { Entity } from '../../models/entity';
 
 @Component({
   selector: 'page-about',
@@ -8,16 +9,18 @@ import {ElasticsearchProvider} from "../../providers/elasticsearch/elasticsearch
 })
 export class AboutPage {
 
-  results: String = "";
+  entities: Entity[];
 
   constructor(public navCtrl: NavController, public elasticsearch: ElasticsearchProvider) {
-
+    this.entities = [];
   }
 
   onInput(event){
     this.elasticsearch.fullTextSearch('tracker', '*' + event.target.value + '*').then(
-      response => {
+      (response) => {
         for (let result of response.hits.hits) {
+          const entity = result._source;
+          this.entities.push(new Entity(entity.entity, entity.entity_name, entity.entity_category))
           console.log(result._source.entity);
         }
       }, error => {
