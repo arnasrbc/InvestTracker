c63ade4e8fde1e2f98dcf6e65503b34511ace321
Pedro Marce Rodriguez <pedromarcerodriguez@Pedros-MacBook.local>
2018-01-08 13:14:20 +0100

    Add firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,22 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
+import { HttpModule } from '@angular/http';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseProvider } from './../providers/firebase/firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBt9o6Dr_8tlIm5HKUl_le4cfUT8eP0Cuw",
+  authDomain: "hackathon-60430.firebaseapp.com",
+  databaseURL: "https://hackathon-60430.firebaseio.com",
+  projectId: "hackathon-60430",
+  storageBucket: "hackathon-60430.appspot.com",
+  messagingSenderId: "700145879292"
+};
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,6 +37,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -34,7 +53,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FirebaseProvider
   ]
 })
 export class AppModule {}
