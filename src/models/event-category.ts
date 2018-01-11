f8df0bebc124d49cfe119d4fcc686b4c4e4c1b9c
mollermariane <mollermariane@gmail.com>
2018-01-11 15:07:03 +0100

    new events

--- src/models/event-category.ts
+++ src/models/event-category.ts
@@ -11,14 +11,29 @@ export const EVENT_CATEGORIES: EventCategroy[] = [
         code: 'new_share_class'
     },
     {
-        icon: 'alert',
-        label: 'Liquidated Share Class',
-        code: 'liquidated_share_class'
+        icon: 'add-circle',
+        label: 'New Legal Found',
+        code: 'new_legal_fund'
     },
     {
         icon: 'add-circle',
-        label: 'New Legal Found ',
-        code: 'new_legal_fund'
+        label: 'New Holding',
+        code: 'new_holding'
+    },
+    {
+        icon: 'add-circle',
+        label: 'New Dealer',
+        code: 'new_dealer'
+    },
+    {
+        icon: 'add-circle',
+        label: 'New Trade',
+        code: 'new_trade'
+    },
+    {
+        icon: 'alert',
+        label: 'Liquidated Share Class',
+        code: 'liquidated_share_class'
     },
     {
         icon: 'warning',
@@ -29,6 +44,26 @@ export const EVENT_CATEGORIES: EventCategroy[] = [
         icon: 'trending-up',
         label: 'Dealer Balance over threshold',
         code: 'dealer_balance'
+    },
+    {
+        icon: 'trending-up',
+        label: 'Holding Balance over threshold',
+        code: 'holding_balance'
+    },
+    {
+        icon: 'trending-up',
+        label: 'Share Class Balance over threshold',
+        code: 'share_class_balance'
+    },
+    {
+        icon: 'trending-up',
+        label: 'Trade amount over threshold',
+        code: 'high_trade_amount'
+    },
+    {
+        icon: 'trending-down',
+        label: 'Trade amount below threshold',
+        code: 'low_trade_amount'
     }
 
 ];
